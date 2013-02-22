0cf6ab089bc61dd499eb26efc390052ed3bf5362
Pavel Chuprikov <pschuprikov@gmail.com>
2013-02-22 15:07:57 +0400

    segal: fixing formulas for second homework.

--- segal/hw2/implicit_downstream.m
+++ segal/hw2/implicit_downstream.m
@@ -1,4 +1,4 @@
 function vs = implicit_downstream(uu, ae)
-    vs = [1 + uu + 2*ae, -uu - ae, -ae; 
-                      0,       -1,   1];
+    vs = [1 - uu + 2*ae,  -ae, uu - ae ; 
+                      0,   -1,       1];
 end
